675345f521557851f9de50335107099210cc4ab1
Adrian Jodzio <a.jodzio@donders.ru.nl>
2021-04-16 16:57:18 +0200

    commented and tidied code

--- Session4.R
+++ Session4.R
@@ -1,19 +1,23 @@
 # 1. Set up your project by loading your packages, e.g.:
-library(package)
+#load the tidyr package
+library("tidyr")
 
 # 2. Set the working directory and load the data, e.g.:
-setwd("directory")
+setwd("C:\Users\adrjod\git_KPLS\Adrjod\key-practices-git.git")
+#import the data 
 data <- read.delim("data.csv", header = TRUE, sep = ",")
 
-# 3. Make variables ready to work on, e.g.:
+# Select the columns that will be used and create two new columns with the mutate function
 data <- data %>%
   select(ppID, condition, trial, RT) %>%
   mutate(condition = as.factor(condition), target = as.factor(target))
+#remove all missing values 
 data <- na.omit(data)
 
-# 4. Explore data, e.g.:
+# examine the data after tidying it 
 summary(data)
 
-# 5. Analysis (can be a simple analysis), e.g. a one-way ANOVA, within-subjects:
+# 5. Analysis - run a one-way ANOVA, within-subjects:
 anovaRTbycategory <- aov(RT ~category+error(ppID/category), data = analysis_anova)
+# Examine the findings from the ANOVA
 summary(anovaRTbycategory)
